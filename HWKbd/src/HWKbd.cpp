d5afd89caa54694c72b9d4c7e8bbf33dfb90cfe9
Pavel Travnik <39143351+ptravnik@users.noreply.github.com>
2019-10-23 17:35:35 +1000

    Fixed pointer

---
+++ HWKbd/src/HWKbd.cpp
@@ -0,0 +1,290 @@
+#include "Arduino.h"
+#include "HWKbd.h"
+
+//
+// Encoder constructor
+//
+HWKbd_Encoder::HWKbd_Encoder( const uint16_t *c, const byte *m){
+  codepage = c;
+  macropage = m;
+}
+
+
+//
+// returns the number of bytes available in the buffer
+//
+byte HWKbd_Encoder::available( void){
+  return _buflen;
+}
+
+
+//
+// returns the next character from the buffer
+//
+char HWKbd_Encoder::read( void){
+  if( !_buflen) return 0;
+  char c = (char)_buffer[_tail++];
+  _tail = _tail & (HWKBD_BUFFER_SIZE-1);
+  _buflen--;
+  return c;
+}
+
+
+//
+// allows to look into the buffer without changing the counters
+//
+char HWKbd_Encoder::peek( void){
+  if( !_buflen) return 0;
+  return (char)_buffer[_tail];
+}
+
+
+//
+// Converts a scan sharacter into a buffer entry
+// returns the new mode
+//
+byte HWKbd_Encoder::encode( byte r, byte _mode){
+  if( codepage==0 || macropage == 0) return _mode;
+  size_t loc = ((r-1)<<3) + _mode;
+  uint16_t code = pgm_read_word( &codepage[loc]);
+#ifdef HWKBD_DEBUG_
+  Serial.println("Encoder call");
+  Serial.print("Location: ");
+  Serial.println(loc);
+  Serial.print("Code: ");
+  Serial.println(code);
+#endif
+  _mode = (code & 0x7000) >> 12;
+  if( code & 0x8000){
+    char *ptr = (char *)(macropage + (code & 0x0FFF));
+    while( _buflen <= HWKBD_BUFFER_SIZE){
+      char c = pgm_read_byte(ptr++);
+      if( !c) break;
+      _buffer[_head++] = c;
+      _head = _head & (HWKBD_BUFFER_SIZE-1);
+      _buflen++; 
+    }
+    return _mode;
+  }
+  byte byte0 = (byte)((code & 0x0F00) >> 8);
+  byte byte1 = (byte)((code & 0x00FF));
+  if( byte0 && _buflen <= HWKBD_BUFFER_SIZE-2){
+    _buffer[_head++] = byte0;
+    _head = _head & (HWKBD_BUFFER_SIZE-1);
+    _buffer[_head++] = byte1;
+    _head = _head & (HWKBD_BUFFER_SIZE-1);
+    _buflen += 2;
+    return _mode;
+  }
+  if( byte1 && _buflen < HWKBD_BUFFER_SIZE){
+    _buffer[_head++] = byte1;
+    _head = _head & (HWKBD_BUFFER_SIZE-1);
+    _buflen++;
+  }
+  return _mode;  
+}
+
+
+void HWKbd::connect(int pKBDrst, int pKBDclk, int pKBDread, int pLEDrst, int pLEDclk, HWKbd_Encoder *enc){
+  pinMode(pKBDrst, OUTPUT);
+  pinMode(pKBDclk, OUTPUT);
+  pinMode(pKBDread, INPUT);
+  pinMode(pLEDrst, OUTPUT);
+  pinMode(pLEDclk, OUTPUT);
+  pinKBD_reset = pKBDrst;
+  pinKBD_clock = pKBDclk;
+  pinKBD_read  = pKBDread;
+  pinLED_reset = pLEDrst;
+  pinLED_clock = pLEDclk;
+  _encoder = enc;
+#ifdef HWKBD_FAST_PORTS
+  _bitKBD_reset = digitalPinToBitMask(pinKBD_reset);
+  _bitKBD_clock = digitalPinToBitMask(pinKBD_clock);
+  _bitKBD_read  = digitalPinToBitMask(pinKBD_read);
+  _bitLED_reset = digitalPinToBitMask(pinLED_reset);
+  _bitLED_clock = digitalPinToBitMask(pinLED_clock);
+  _prtKBD_reset = (volatile unsigned int *)portOutputRegister(digitalPinToPort(pinKBD_reset));
+  _prtKBD_clock = (volatile unsigned int *)portOutputRegister(digitalPinToPort(pinKBD_clock));
+  _prtKBD_read  = (volatile unsigned int *)portInputRegister(digitalPinToPort(pinKBD_read));
+  _prtLED_reset = (volatile unsigned int *)portOutputRegister(digitalPinToPort(pinLED_reset));
+  _prtLED_clock = (volatile unsigned int *)portOutputRegister(digitalPinToPort(pinLED_clock));
+#endif
+  isConnected = true;
+  setLEDs( 0);
+#ifdef HWKBD_DEBUG_
+  Serial.println( "hwKeyboard connected, ports: ");
+  Serial.print( pinKBD_reset);
+  Serial.print( " ");
+  Serial.print( pinKBD_clock);
+  Serial.print( " ");
+  Serial.print( pinKBD_read);
+  Serial.print( " ");
+  Serial.print( pinLED_reset);
+  Serial.print( " ");
+  Serial.println( pinLED_clock);
+#endif
+}
+
+#ifndef HWKBD_FAST_PORTS
+void HWKbd::_sendPulse(int pin){
+  digitalWrite( pin, HIGH);
+#ifdef HWKBD_PULSE_LEN
+  delayMicroseconds(HWKBD_PULSE_LEN);
+#endif
+  digitalWrite( pin, LOW);
+#ifdef HWKBD_PULSE_LEN
+  delayMicroseconds(HWKBD_PULSE_LEN);
+#endif
+}
+#endif
+
+#ifdef HWKBD_FAST_PORTS
+void HWKbd::setLEDs( byte n){
+  if( !isConnected) return;
+  // Critical section - disable interrupts
+  uint8_t oldSREG = SREG;
+  cli();
+  *_prtLED_reset |= _bitLED_reset; 
+  *_prtLED_reset &= ~_bitLED_reset; 
+  for( uint8_t i=0; i<n; i++){
+    *_prtLED_clock |= _bitLED_clock; 
+    *_prtLED_clock &= ~_bitLED_clock;     
+  }
+  SREG = oldSREG;
+}
+#else
+void HWKbd::setLEDs( byte n){
+  if( !isConnected) return;
+  _sendPulse(pinLED_reset);
+  for( byte i=0; i<n; i++) 
+    _sendPulse(pinLED_clock); 
+}
+#endif
+
+void HWKbd::setMode(byte newmode){
+  uint8_t leds[] = {0,8,2,4,1,9,3,5};
+  if(_mode == newmode) return;  
+  _mode = newmode;
+  if( modeLEDsEnabled)
+    setLEDs(leds[_mode]);
+}
+
+#ifdef HWKBD_FAST_PORTS
+byte HWKbd::scan( void){
+  if( !isConnected) return 0;
+  byte Output = 0;
+  // Critical section - disable interrupts
+  uint8_t oldSREG = SREG;
+  cli();
+  *_prtKBD_reset |= _bitKBD_reset; 
+  *_prtKBD_reset &= ~_bitKBD_reset; 
+  for( uint8_t i=0; i<HWKBD_LAST_BUTTON; i++){
+    if( *_prtKBD_read & _bitKBD_read){
+      Output = i+1;
+      break;
+    }
+    *_prtKBD_clock |= _bitKBD_clock; 
+    *_prtKBD_clock &= ~_bitKBD_clock;     
+  }
+  SREG = oldSREG;
+#ifdef HWKBD_DEBUG_
+  if( !Output) return 0;
+  Serial.print("Scanned: ");
+  Serial.println( Output);
+  delay(50);  
+#endif
+  return Output;
+}
+#else
+
+//
+// scan checks the button immediately and returns the scan code;
+// the total read cycle is better than 60 microseconds if
+// the fast port access is used
+// this method is useful for games
+//
+byte HWKbd::scan( void){
+  if( !isConnected) return 0;
+  byte Output = 0;
+  _sendPulse(pinKBD_reset);
+  for( byte i=0; i<HWKBD_LAST_BUTTON; i++){
+    if( digitalRead( pinKBD_read)){
+      Output = i+1;
+      break;
+    }
+    _sendPulse(pinKBD_clock);
+  }
+#ifdef HWKBD_DEBUG_
+  if( !Output) return 0;
+  Serial.print("Scanned: ");
+  Serial.println( Output);
+  delay(50);  
+#endif
+  return Output;
+}
+#endif
+
+byte HWKbd::_debounce( void){
+  byte r = scan();
+  long t = millis();
+  
+  // no button pressed
+  if( !r && !buttonPressed){
+    return 0;
+  }
+
+  // button apparently not pressed, but it was
+  // means the button is bouncing
+  if( !r && buttonPressed){
+    lastReleaseTime = t;
+    buttonPressed = false;
+    return lastScanResult;
+  }
+  
+  // new press
+  if( r && !buttonPressed){
+    if( lastScanResult != r){
+      lastScanResult = r;
+      lastPressTime = t;
+      buttonPressed = true;
+      return 0;
+    }
+    else{
+      buttonPressed = (t-lastReleaseTime) > HWKBD_BUTTON_REPEAT;
+      if(buttonPressed) lastPressTime = t;
+      return 0;      
+    }
+  }
+  
+  // holding the button
+  if( r && buttonPressed){
+    if( lastScanResult != r){
+      byte tmp = lastScanResult;
+      lastReleaseTime = t;
+      lastPressTime = t;
+      lastScanResult = r;
+      return tmp;
+    }
+    if( (t-lastPressTime) > HWKBD_TYPEMATIC_DELAY){
+      delay(HWKBD_BUTTON_REPEAT);
+      return lastScanResult;
+    }
+  }
+  return 0;
+}
+
+//
+// input registers the key with proper debouncing
+// the input is committed if the button is released
+// or if the button is held down for time longer than
+// the TYPEMATIC_DELAY
+//
+bool HWKbd::input( void){
+  byte r = _debounce();
+  if( r == 0) return false;
+  lastButtonRegistered = r;
+  if( r > HWKBD_LAST_CODEPAGE) return true;
+  byte newMode = _encoder->encode( r, _mode);
+  setMode(newMode);
+  return true;
+}
---
+++ HWKbd/src/HWKbd.h
@@ -0,0 +1,157 @@
+#ifndef HWKbd_h
+#define HWKbd_h
+
+/*
+  HWKbd.h - Hardware keyboard for Arduino with an SPI-like interface
+  Copyright (c) 2019 Pavel Travnik.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Hardware Notes.
+  
+  The keyboard communicates with Arduino via J1:
+  1. POWER - connect to +5 V on Mega or +3.3V on Duo
+  2. KBD_RESET - output, any digital pin
+  3. KBD_CLOCK - output, any digital pin
+  4. KBD_READ - input (no pullup!), any digital pin
+  5. LED_RESET - output, any digital pin
+  6. LED_CLOCK - output, any digital pin
+  7. RESET - connect to Arduino RESET (optional)
+  8. GROUND
+  To read the keyboard, send a pulse to KBD_RESET and read
+  the first button state from KBD_READ (HIGH means "pressed").
+  Then, continue sending pulses on KBD_CLOCK to read buttons
+  2, 3, 4, etc.
+  To set the LEDs, send a pulse to LED_RESET and cycle
+  LED_CLOCK from 0 to 15 times, which corresponds to 16 binary states.
+  Note that the software presimes that no PWM is sent to the keyboard ports!
+
+  Software Notes.
+  The keyboard communicates in UTF-8. The current implementation supports
+  8 reading modes:
+  0. English character set, small letters, numbers
+  1. SHIFT. English character set, capital letters, punctuation marks.
+  2. F1 - additional letters and macros in English
+  3. F2 - additional letters and macros in English
+  4. LANG - national language mode (currently only Cyrillic is implemented)
+  5. SHIFT+LANG - capital letters for Cyrillic
+  6. F1+LANG - additional letters and macros in national language
+  7. F2+LANG - additional letters and macros in national language
+  
+  The key codes are composed of 16-bit integers, 8 numbers per button
+  (by the number of modes). The bit pattern is as following:
+  0MMM0000 cccccccc
+  1MMMaaaa aaaaaaaa
+  MMM - the next mode (from 0 to 7)
+  cccccccc - a single-byte Unicode character (if >128, it is a control character, such as an arrow)
+  aaaaa aaaaaaaaa - a 12-bit shift in the macros table;
+  the latter is composed of null-terminated strings, up to 4k in length.
+*/
+
+// uncomment for debugging
+//#define HWKBD_DEBUG_
+
+// delay before the repeat starts and the repeat rate in ms
+#define HWKBD_TYPEMATIC_DELAY 200
+#define HWKBD_BUTTON_REPEAT 50
+
+// comment out for standard Arduino library writing
+// (including timer disable on PWM)
+// will slow down everything by a factor of 20
+#define HWKBD_FAST_PORTS
+
+// pulse width in microseconds
+#define HWKBD_PULSE_LEN 1
+
+// keyboard circular buffer;
+// must be a power of 2 and less than 256
+#define HWKBD_BUFFER_SIZE 32
+
+// number of buttons (maximum is 80)
+#define HWKBD_LAST_BUTTON 59
+#define HWKBD_LAST_CODEPAGE 59
+
+
+#include <Arduino.h>
+#ifdef __AVR__
+#include <avr/pgmspace.h>
+#else
+#include <pgmspace.h>
+#endif
+
+
+class HWKbd_Encoder
+{  
+  public:
+    const uint16_t *codepage = 0;
+    const byte *macropage = 0;
+    HWKbd_Encoder( const uint16_t *c, const byte *m);
+    byte encode( byte r, byte _mode);
+    byte available(void);
+    char read(void);
+    char peek(void);
+  private:
+    volatile char _buffer[HWKBD_BUFFER_SIZE];
+    volatile uint8_t _head = 0;
+    volatile uint8_t _tail = 0;
+    volatile uint8_t _buflen = 0;
+};
+
+
+class HWKbd
+{    
+  public:
+    bool isConnected = false;
+    int pinKBD_reset = -1;
+    int pinKBD_clock = -1;
+    int pinKBD_read  = -1;
+    int pinLED_reset = -1;
+    int pinLED_clock = -1;
+    volatile bool modeLEDsEnabled = true;
+    volatile byte lastScanResult = 0;
+    volatile long lastReleaseTime = 0L;
+    volatile long lastPressTime = 0L;
+    volatile bool buttonPressed = false;
+    volatile char lastButtonRegistered = 0;
+    void connect(int pKBDrst, int pKBDclk, int pKBDread, int pLEDrst, int pLEDclk, HWKbd_Encoder *enc);
+    void setLEDs( byte n);
+    void setMode( byte newmode);
+    byte scan( void);
+    bool input( void);
+    inline byte available(void){ return _encoder->available();};
+    inline char read(void){ return _encoder->read();};
+    inline char peek(void){ return _encoder->peek();};
+
+  private:
+    volatile uint8_t _mode = 0;
+#ifdef HWKBD_FAST_PORTS
+    uint8_t _bitKBD_reset;
+    uint8_t _bitKBD_clock;
+    uint8_t _bitKBD_read;
+    uint8_t _bitLED_reset;
+    uint8_t _bitLED_clock;
+    volatile unsigned int *_prtKBD_reset;
+    volatile unsigned int *_prtKBD_clock;
+    volatile unsigned int *_prtKBD_read;
+    volatile unsigned int *_prtLED_reset;
+    volatile unsigned int *_prtLED_clock;
+#else
+    void _sendPulse(int pin);
+#endif
+    HWKbd_Encoder *_encoder; 
+    byte _debounce( void);
+};
+
+#endif
