f057b8301f30b9783b4c0a4c2eeb2a8a4a673c20
Pavel Travnik <39143351+ptravnik@users.noreply.github.com>
2020-03-08 15:06:20 +1000

    First working version

---
+++ Keyboard_Injector_Micro/CP1251_mod.h
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////
+//
+//  RetroRPN - "Электроника МК-90" reborn
+//  Copyright (c) 2019 Pavel Travnik.  All right reserved.
+//  See main file for the license
+//
+//////////////////////////////////////////////////////////
+
+//
+// Characters defined as bytes (unsigned 8-bits)
+//
+
+#ifndef CP1254MOD_H
+#define CP1254MOD_HPP
+
+#define _NUL_        0
+#define _BEL_        7
+#define _BS_         8
+#define _TAB_        9
+#define _LF_        10
+#define _CR_        13
+#define _LEFT_      17
+#define _RIGHT_     18
+#define _UP_        19
+#define _DOWN_      20
+#define _HOME_      21
+#define _END_       22
+#define _PGUP_      23
+#define _PGDN_      24
+#define _ESC_       27
+#define _RPN_       31
+#define _SP_        32
+#define _ZERO_      48
+#define _COLON_     58
+#define _DEL_      127
+#define _PLSMNS_   177
+
+// Keyboard and mouse 
+#define _KBD_INJ_    1
+#define _MOUSE_INJ_  2
+
+// Unknown UTF is a star!
+#define UNKNOWN_UTF8 '*'
+
+#endif
--- Keyboard_Injector_Micro/Keyboard_Injector_Micro.ino
+++ Keyboard_Injector_Micro/Keyboard_Injector_Micro.ino
@@ -1,8 +1,11 @@
 #include <SoftwareSerial.h>
-//#include "Keyboard.h"
-//#include "Mouse.h"
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "CP1251_mod.h" 
 
-#define __DEBUG
+// Uncomment for debugging
+//#define __DEBUG
+//#define __DEBUG_CODES
 
 #define PC_BAUD 115200
 #define SERIAL_BAUD 19200
@@ -26,7 +29,31 @@ static volatile byte MouseCommandPosition = 0;
 static volatile long lastActive = 0;
 static volatile long lastBlinked = 0;
 
+#define UTF8_BUFFER_LENGTH 64
+static byte UTF8_buffer[UTF8_BUFFER_LENGTH];
+static byte CP1251_buffer[UTF8_BUFFER_LENGTH];
+
+#define N_SUBS 3
+const byte _PLSMNS_SUB[] PROGMEM = { '+', '/', '-', _NUL_};
+const byte _YO_SUB[] PROGMEM = { 0xD1, 0x91, _NUL_};
+const byte _YO_CAP_SUB[] PROGMEM = { 0xD0, 0x81, _NUL_};
+
+const byte _CP1251_bytes[] PROGMEM = {
+  _PLSMNS_, 
+  0xB8, // small "YO"
+  0xA8  // capital "YO"
+  };
+const byte *const _CP1251_substitutes[] PROGMEM = {
+  _PLSMNS_SUB, 
+  _YO_SUB, 
+  _YO_CAP_SUB, 
+  };
+
+//
+// Runs once, upon the power-up
+//
 void setup() {
+  memset( UTF8_buffer, 0, UTF8_BUFFER_LENGTH);
   Serial.begin(PC_BAUD);
   while(Serial.available()) Serial.read();
   mySerial.begin(SERIAL_BAUD);
@@ -35,8 +62,8 @@ void setup() {
   digitalWrite( ACTIVITY_TO_ESP32, LOW);
   pinMode( ACTIVITY_LED, OUTPUT);
   digitalWrite( ACTIVITY_LED, LOW);
-  //Mouse.begin();
-  //Keyboard.begin();
+  Mouse.begin();
+  Keyboard.begin();
   delay(100);
   blink_Activity();
   delay(300);
@@ -49,68 +76,24 @@ void setup() {
   return;
 }
 
+//
+// Runs continuously
+//
 void loop() {
   char c = 0;
   blink_Activity();
   if( Serial.available()){
     lastActive = millis();
     c = Serial.read();
-    if( c != 0){
-      #ifdef __DEBUG
-      // loopback
-      Serial.write(c);
-      #endif
-      mySerial.write(c);
-    }
+    if( c != 0)
+      sendToESP32Serial(c);
   }
   if( mySerial.available()){
     lastActive = millis();
     c = mySerial.read();
-    if( c != 0){
-      Serial.write(c);
-    }
+    if( !sendToPCKeyboard(c))
+      sendToPCSerial(c);
   }
-
-  //if( mySerial.available()){
-  //  c = mySerial.read();
-  //  Serial.write(c);
-  //}
-//    switch( c){
-//      case 0:
-//        return;
-//      case 1:
-//        KeyboardWrite = !KeyboardWrite;
-//        return;
-//      case 2:
-//        KeyboardPress = !KeyboardPress;
-//        return;
-//      case 3:
-//        KeyboardRelease = !KeyboardPress;
-//        return;
-//      case 4:
-//        resetKeyboardPrefix();
-//        Keyboard.releaseAll();
-//        return;
-//      default:
-//        if( KeyboardWrite){
-//          Keyboard.write(c);
-//          resetKeyboardPrefix();
-//          return;
-//        }
-//        if( KeyboardPress){
-//          Keyboard.press(c);
-//          resetKeyboardPrefix();
-//          return;
-//        }
-//        if( KeyboardRelease){
-//          Keyboard.release(c);
-//          resetKeyboardPrefix();
-//          return;
-//        }
-//        mySerial.write(c);
-//        return;
-//    }
-//  }
 
   // Eliminate unwanted Tx, Rx lights
   //RXLED1;
@@ -135,8 +118,301 @@ void blink_Activity(){
   lastBlinked = millis();
 }
 
-//void resetKeyboardPrefix(){
-//  KeyboardWrite = false;
-//  KeyboardPress = false;
-//  KeyboardRelease = false;
-//}
+//
+// Converts a character from CP1251 into Unicode
+// (e.g. for printing via UTF8 console)
+//
+char *_setDuplet( byte *buff, byte c){
+  *buff++ = c;
+  *buff = _NUL_;
+  return (char *)buff;  
+}
+char *_setTriplet( byte *buff, byte prefix, byte c){
+  *buff++ = prefix;
+  *buff++ = c;
+  *buff = _NUL_;
+  return buff;  
+}
+char *_setMacro( byte *buff, const char *macro){
+  for( byte i=0; i<UTF8_BUFFER_LENGTH-1; i++){
+    char c = pgm_read_byte_near(macro + i);
+    if( c == 0) break;
+    *buff++ = c;
+  }
+  *buff = _NUL_;
+  return (char *)buff; 
+}
+char *convertToUTF8( byte *buff, byte c){
+  if( c < 128) return _setDuplet(buff, c);
+  for( byte i=0; i<N_SUBS; i++){
+    if( c == pgm_read_byte_near(_CP1251_bytes + i))
+      return _setMacro( buff, _CP1251_substitutes[i]);
+    }
+  if( c >= 0xF0) return _setTriplet(buff, 0xD1, c - 0x70); // small alpha
+  if( c >= 0xC0) return _setTriplet(buff, 0xD0, c - 0x30); // large alpha
+  return _setTriplet(buff, 0xD1, c);
+}
+
+//
+// Sends characters to PC serial port
+// Returns the number of chars sent
+//
+byte sendToPCSerial(char c){
+  *UTF8_buffer = _NUL_; 
+  convertToUTF8(UTF8_buffer, (byte)c);
+  char *ptr = (char *)UTF8_buffer;
+  for( byte i=0; i<UTF8_BUFFER_LENGTH; i++){
+    if( *ptr == _NUL_) return i;
+    Serial.write( *ptr++);
+  }
+  return UTF8_BUFFER_LENGTH-1;
+}
+
+//
+// Converts the keyboard injection command
+// Returns true if injected
+//
+bool sendToPCKeyboard( char c){
+  if( c != _KBD_INJ_ && c != _MOUSE_INJ_) return false;
+  if( c == _KBD_INJ_) return processKbdInject();
+  return processMouseInject();
+}
+bool processKbdInject(){
+  for( byte i=0; i<10; i++){
+    if( !mySerial.available()){
+      delay(10);
+      continue;
+    }
+    byte c = (byte)mySerial.read();
+    if( c < _BEL_) return false;
+    Keyboard.write(c);
+    return true;
+  }
+  // no command in 100 ms: misplaced inject
+  return false;
+}
+bool processMouseInject(){
+  for( byte i=0; i<10; i++){
+    if( !mySerial.available()){
+      delay(10);
+      continue;
+    }
+    byte c = (byte)mySerial.read();
+    byte command = c & 0x03; // two lower bytes are the command
+    c >>= 2;
+    int delta = (int)c - 31; 
+    switch(command){
+      case 0: // buttons click
+        if( c & 0x1) Mouse.click(MOUSE_LEFT);
+        if( c & 0x2) Mouse.click(MOUSE_RIGHT);
+        if( c & 0x3) Mouse.click(MOUSE_MIDDLE);
+        return true;
+      case 1: // movement x
+        Mouse.move( delta, 0, 0);
+        return true;
+      case 2: // movement y
+        Mouse.move( 0, delta, 0);
+        return true;
+      case 3: // movement z
+        Mouse.move( 0, 0, delta);
+        return true;
+      default:
+        break;
+    }
+    return true;
+  }
+  // no command in 100 ms: misplaced inject
+  return false;
+}
+
+//
+// Converts message from Unicode to CP1251
+//
+#ifdef __DEBUG
+void _reportUnknownUnicode( byte *ptr){
+  ptr--;
+  Serial.print( "Unknown unicode: ");
+  Serial.print( *ptr++, HEX);        
+  Serial.print( " ");        
+  Serial.print( *ptr++, HEX);        
+  Serial.print( " ");        
+  Serial.print( *ptr++, HEX);        
+  Serial.print( " ");        
+  Serial.println( *ptr++, HEX);        
+};
+#endif
+byte *_addCh( byte* ptr, byte* ptr_limit, byte c){
+  if( ptr >= ptr_limit) return ptr_limit;
+  *ptr++ = c;
+  return ptr;
+}
+char *convertToCP1251( byte *buff, char *message, size_t limit){
+  byte c;
+  byte Unicode_Prefix = 0;
+  byte *ptr1 = buff; 
+  byte *ptr2 = (byte *)message; 
+  byte *ptr_limit = ptr1 + limit - 1; 
+  for (byte i=0; i<strlen(message); i++) {
+    c = *ptr2++;
+    switch(Unicode_Prefix){
+      case 0: // No prefix
+        if( !c) // safety termination
+          return _addCh(ptr1, ++ptr_limit, (byte)0);
+        if( c < 128){ // lower chars
+          ptr1 = _addCh(ptr1, ptr_limit, c);
+          break;
+        }
+        Unicode_Prefix = c;
+        break;
+      case 0xD0:
+        Unicode_Prefix = 0;
+        if( 0x90 <= c && c < 0xD0){
+          ptr1 = _addCh(ptr1, ptr_limit, c + 0x30);
+          break;
+        }
+        if( c == 0x81){
+          ptr1 = _addCh(ptr1, ptr_limit, 0xA8);
+          break;
+        }
+        ptr1 = _addCh(ptr1, ptr_limit, c);
+        break;
+      case 0xD1:
+        Unicode_Prefix = 0;
+        if( 0x80 <= c && c < 0x90){
+          ptr1 = _addCh(ptr1, ptr_limit, c + 0x70);
+          break;
+        }
+        if( c == 0x91){
+          ptr1 = _addCh(ptr1, ptr_limit, 0xB8);
+          break;
+        }
+        ptr1 = _addCh(ptr1, ptr_limit, c);
+        break;
+      case 0xC2:
+        Unicode_Prefix = 0;
+        if( c == 0xAB || c == 0xBB){ // quote
+          ptr1 = _addCh(ptr1, ptr_limit, '\"');
+          break;
+        }
+        #ifdef __DEBUG
+        _reportUnknownUnicode( ptr2);
+        #endif
+        ptr1 = _addCh(ptr1, ptr_limit, UNKNOWN_UTF8);
+        break;
+      case 0xE2:
+        Unicode_Prefix = 0;
+        if( c == 0x80 && *ptr2 == 0x94){ // EM-dash
+          ptr1 = _addCh(ptr1, ptr_limit, '-');
+          ptr1 = _addCh(ptr1, ptr_limit, '-');
+          ptr2++;
+          break;
+        }
+        if( c == 0x80 && *ptr2 == 0xA6){ // triple dot
+          ptr1 = _addCh(ptr1, ptr_limit, '.');
+          ptr1 = _addCh(ptr1, ptr_limit, '.');
+          ptr1 = _addCh(ptr1, ptr_limit, '.');
+          ptr2++;
+          break;
+        }
+        #ifdef __DEBUG
+        _reportUnknownUnicode( ptr2);
+        #endif
+        ptr1 = _addCh(ptr1, ptr_limit, UNKNOWN_UTF8);
+        ptr2++;
+        break;
+      default: // Unknown Unicode is replaced with *
+        Unicode_Prefix = 0;
+        #ifdef __DEBUG
+        _reportUnknownUnicode( ptr2);
+        #endif
+        ptr1 = _addCh(ptr1, ptr_limit, UNKNOWN_UTF8);
+        break;
+    }
+  } // for
+  // safe string termination
+  return _addCh(ptr1, ++ptr_limit, (byte)0);
+}
+
+//
+// Sends characters to ESP32 serial port
+// Conversion from UTF8 to CP1251 performed
+// Returns the number of chars sent
+//
+byte sendToESP32Serial(char cc){
+  byte c = (byte)cc;
+  if( 0 < c && c < 128){
+    #ifdef __DEBUG
+    // loopback
+    Serial.write(cc);
+    #endif
+    mySerial.write(cc);
+    return 1;
+  }
+  char *ptr = (char *)UTF8_buffer;
+  *ptr++ = cc;
+  *ptr = _NUL_;
+  switch( c){
+    case _NUL_:
+      return 0;
+    case 0xD0: // double character (Russian letters)
+    case 0xD1: // double character (Russian letters)
+    case 0xC2: // double character (quotations)
+      if( !readNonlocking(ptr)) return 0;
+      return _sendToESP32();
+    case 0xE2: // triple character (Ellipsis and EM-dash)
+      if( !readNonlocking(ptr++)) return 0;
+      if( !readNonlocking(ptr)) return 0;
+      return _sendToESP32();
+    default: // unknown or misplaced Unicode is a star!
+      #ifdef __DEBUG
+      // loopback
+      Serial.write(UNKNOWN_UTF8);
+      #endif
+      mySerial.write(UNKNOWN_UTF8);
+      break;
+  }
+  return 0;
+}
+bool readNonlocking(byte *ptr){
+  for( byte i=0; i<10; i++){
+    if( !Serial.available()){
+      delay(10);
+      continue;
+    }
+    *ptr++ = (byte)Serial.read();
+    *ptr = _NUL_;
+    return true;
+  }
+  return false;
+}
+byte _sendToESP32(){
+  #ifdef __DEBUG
+  // loopback
+  Serial.println( (char *)UTF8_buffer);
+  #endif
+  #ifdef __DEBUG_CODES
+  Serial.print( "From [");
+  for( byte i=0; i<UTF8_BUFFER_LENGTH; i++){
+    byte c = UTF8_buffer[i];
+    if( c == _NUL_) break;
+    Serial.print( c, HEX);    
+  }
+  Serial.println( "]");
+  #endif
+  convertToCP1251( CP1251_buffer, (char *)UTF8_buffer, UTF8_BUFFER_LENGTH-1);
+  #ifdef __DEBUG_CODES
+  Serial.print( "To [");
+  for( byte i=0; i<UTF8_BUFFER_LENGTH; i++){
+    byte c = CP1251_buffer[i];
+    if( c == _NUL_) break;
+    Serial.print( c, HEX);    
+  }
+  Serial.println( "]");
+  #endif
+  char *ptr = (char *)CP1251_buffer;
+  for( byte i=0; i<UTF8_BUFFER_LENGTH; i++){
+    if(*ptr == 0) return i;
+    mySerial.write( *ptr++);        
+  }
+  return UTF8_BUFFER_LENGTH-1;
+}
